622f3c6d0d8d77ecaa4566142c4ca0512bc6ab98
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-06-06 19:45:55 +0500

    💛 JS START

---
+++ 03.JavaScript/01.intro/app.js
@@ -0,0 +1,20 @@
+alert("hello");
+
+prompt("Please enter your name"); // input / user se data lete hyn
+
+console.log("Hello World");
+console.log(5 + 5);
+
+let myName = "shehzad iqbal";
+
+console.log(myName);
+console.log(myName);
+console.log(myName);
+
+// ----------------
+
+let username = prompt("enter your name");
+alert("Welcome " + username);
+
+// // This is "Hello world"
+// console.log("This is "Hello ");
