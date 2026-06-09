ffcdbdbe78de6212ac9b36fe6172e8fcdce728be
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-06-10 02:24:13 +0500

    CLASS: var vs let vs const, arithmetic operator

---
+++ 03.JavaScript/02.arithmetics/app.js
@@ -0,0 +1,31 @@
+// https://www.w3schools.com/js/js_arithmetic.asp
+
+console.log(6 + 3);
+console.log(6 - 2);
+console.log(6 * 2);
+console.log(13 / 5);
+console.log(13 % 5); // Modulus humme Remainder batata hy
+console.log(17 % 6); // Modulus humme Remainder batata hy
+console.log(23 % 7); // Modulus humme Remainder batata hy
+console.log(6 ** 17); // Exponentiation
+
+// -------------------
+var a = 5; // old way
+
+console.log(a);
+
+a = 6;
+
+console.log(a);
+
+// const me reassignment nhi hota
+const zakat = 2.5;
+
+const result = "smit";
+
+let a = 5;
+let A = 6;
+
+console.log(a);
+
+let Let = 6; // no keywords
