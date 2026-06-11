ca70491a0844807f937965af63cf80019400fcc9
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-06-11 19:30:21 +0500

    JS CLASS: data types, dry run, bodmas

---
+++ 03.JavaScript/03.data-types/shehzad.js
@@ -0,0 +1,47 @@
+// alert("Hello \n world")
+
+// this is blocking code (mtlb isky baad aagye code nhi chale ga)
+
+// console.log("hello")
+
+// BBSSNNU (BigInt, Boolean, String, Symbol, Number, Null, Undefined)
+
+// let test = 48534384934384348
+// let b =  485343849343843488457454n
+// let b = BigInt(485343849343843488457454)
+
+// int a = 5
+// float a = 5.6
+
+// console.log(typeof b);
+
+// let num = 5847348934
+// let text = "hello"
+// let isPresent = true (han ya na)
+// let b = undefined
+// let courseResult = null  (empty variable)
+
+// let s = Symbol("hello")
+
+// console.log(typeof(isPresent));
+
+console.log(5 + 4); // 9
+console.log("5 + 4"); // 5 + 4
+console.log("5" + "4"); // 54
+
+// console.log("Shehzad" + "Iqbal");
+
+/*
+multi line comment
+
+ye
+hy
+bara
+comment
+
+*/
+
+// document.write("smit")
+
+console.log(2 + 3 * 4);
+// BODMAS
