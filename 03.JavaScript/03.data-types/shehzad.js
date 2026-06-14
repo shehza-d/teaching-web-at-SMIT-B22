4f79c376be5b4d84fffdf67bc971b1f47a45915a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-06-14 10:35:28 +0500

    class: increment

--- 03.JavaScript/03.data-types/shehzad.js
+++ 03.JavaScript/03.data-types/shehzad.js
@@ -1,35 +1,48 @@
-// alert("Hello \n world")
+
+// Primitive data types are 7
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. undefined
+// 5. null
+// 6. BigInt
+// 7. Symbol
+
+let module_one_passed_students; // snake case
+let moduleOnePassedStudents; // camel case
+// let module-one-passed-students; // kebab case
+
+
+
+alert("Hello \n world");
 
 // this is blocking code (mtlb isky baad aagye code nhi chale ga)
 
-// console.log("hello")
+console.log("hello");
 
-// BBSSNNU (BigInt, Boolean, String, Symbol, Number, Null, Undefined)
+BBSSNNU(BigInt, Boolean, String, Symbol, Number, Null, Undefined);
 
-// let test = 48534384934384348
-// let b =  485343849343843488457454n
-// let b = BigInt(485343849343843488457454)
+let test = 48534384934384348;
+let b = 485343849343843488457454n;
+let b = BigInt(485343849343843488457454);
 
-// int a = 5
-// float a = 5.6
+console.log(typeof b);
 
-// console.log(typeof b);
+let num = 5847348934;
+let text = "hello";
+let isPresent = true; // (han ya na)
+let b = undefined;
+let courseResult = null; // (empty variable)
 
-// let num = 5847348934
-// let text = "hello"
-// let isPresent = true (han ya na)
-// let b = undefined
-// let courseResult = null  (empty variable)
+let s = Symbol("hello");
 
-// let s = Symbol("hello")
-
-// console.log(typeof(isPresent));
+console.log(typeof isPresent);
 
 console.log(5 + 4); // 9
 console.log("5 + 4"); // 5 + 4
 console.log("5" + "4"); // 54
 
-// console.log("Shehzad" + "Iqbal");
+console.log("Shehzad" + "Iqbal");
 
 /*
 multi line comment
@@ -41,7 +54,27 @@ comment
 
 */
 
-// document.write("smit")
+document.write("smit");
 
 console.log(2 + 3 * 4);
 // BODMAS
+
+// TODO -----------------
+
+console.log("Test " + 2 - 2);
+
+console.log("2" + 2 - 2);
+
+console.log("2" - 2 + 2);
+
+console.log("Test" - 2 + 2);
+
+console.log("Test" + 2 * 2);
+
+console.log(("Test" + 2) * 2);
+
+console.log("Power ", 3 * 5 ** 4 * 2);
+
+console.log((3 + 4) * 5);
+
+console.log(4 * (4 - 55) + 60 / (-4 * 55));
---
+++ 03.JavaScript/04.increment-operator/app.js
@@ -0,0 +1,21 @@
+let a  =  -5;
+
+// console.log(--a);
+
+// a++ // post increment
+// ++a // pre  increment
+// a-- // post decrement
+// --a // pre  decrement
+
+
+let salary = 10000;
+
+salary = 2000
+
+console.log(salary);
+
+// 5 > 3 > 2
+
+// let x = 0b3453 // binary
+
+console.log(x)
