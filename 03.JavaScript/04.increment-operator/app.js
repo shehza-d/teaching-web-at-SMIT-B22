cd5feb58c8f62a9ee0fa437c0cb68a61d057a4f5
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-06-18 12:42:22 +0500

    CLASS: increment decrement, assignment

--- 03.JavaScript/04.increment-operator/app.js
+++ 03.JavaScript/04.increment-operator/app.js
@@ -1,21 +1,97 @@
-let a  =  -5;
+let a = 5;
 
-// console.log(--a);
+console.log(a++);
 
-// a++ // post increment
-// ++a // pre  increment
-// a-- // post decrement
-// --a // pre  decrement
+console.log(++a);
 
+console.log(a + 3); // 8
 
-let salary = 10000;
+a++; // post increment
+++a; // pre  increment
+a--; // post decrement
+--a; // pre  decrement
 
-salary = 2000
+let salary = 15000;
+
+salary = salary + 2000;
+salary += 2000;
 
 console.log(salary);
 
-// 5 > 3 > 2
+5 > 3 > 2;
 
-// let x = 0b3453 // binary
+// Decimal → Binary
+(12).toString(2); // "1100"
 
-console.log(x)
+let x = 0b1101; // binary (13)
+
+console.log(x);
+
+let score = 51;
+
+score = score + 5;
+score += 5;
+score++; // adds 1 only
+
+console.log(score);
+
+let a = 5;
+let b = 8;
+
+b = ++a;
+
+console.log("a:", a);
+console.log("b:", b);
+
+let nr1 = 4;
+let nr2 = 5;
+let nr3 = 2;
+
+console.log(nr1++ + ++nr2 * nr3++); // output batao
+
+console.log((2 + 3) * 4);
+
+// ------
+
+let userInputNumber = 4;
+let myNum = 10;
+
+myNum = myNum + 23 * 5 - 2 / userInputNumber;
+
+console.log("myNum: ", myNum);
+
+let userInputNumber = 17;
+
+let result = userInputNumber % 5;
+console.log("result: ", result);
+
+// -------------
+
+let userInputNumber = 23;
+
+userInputNumber = userInputNumber + 1;
+userInputNumber += 1;
+userInputNumber++;
+userInputNumber++;
+userInputNumber--;
+
+console.log("userInputNumber: ", userInputNumber);
+
+let input = 5;
+
+let result = input + 1 + ++input + input++ + --input + input--;
+// let result = 5 + 1 + 6 + 6 + 6 + 6;
+
+console.log("result: ", result);
+console.log("input: ", input);
+
+// --------
+
+let a = 5;
+let b = 3;
+
+let result = a + b-- * ++a + b / --b - a++;
+
+console.log("result: ", result);
+console.log("a: ", a);
+console.log("b: ", b);
