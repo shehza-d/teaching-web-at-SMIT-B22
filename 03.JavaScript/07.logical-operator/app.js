580cc2d51f070c0a12b529aeff1e9e94313cd6d3
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-06-30 21:22:31 +0500

    class: logical and if else

--- 03.JavaScript/07.logical-operator/app.js
+++ 03.JavaScript/07.logical-operator/app.js
@@ -1,65 +1,55 @@
 // https://www.w3schools.com/js/js_logical.asp
 
+let age = 55;
+
+console.log(!(age > 18));
+console.log(!0);
+console.log(!5);
+console.log(!"false");
 
 // OR Operator (ya)
-// let test1 = true || true;
-// console.log(test1);
+let test1 = true || true;
 
-// let food = true;
-// let tea = true;
-// let drink = true;
-
-// if (food || tea || drink) {
-//   console.log("le aao");
-// } else {
-//   console.log("ghar jao");
-// }
+console.log(test1);
 
 // AND Operator (aur)
 let test2 = true && false && true;
 
-// console.log(test2);
-
-// let isEnroll = true;
-// let passed = true;
-// let age = 20;
-
-// if (isEnroll && passed && age > 18) {
-//   console.log("Pharai karo class ma");
-// } else {
-//   console.log("ghar jao");
-// }
+console.log(test2);
 
 let x = 5;
 let y = 16;
 
-// console.log(x == 5 || y == 5);
-// console.log(5 < 10 && y > 1);
-// console.log(!(x === y));
+console.log(x == 5 || y == 5);
+console.log(5 < 10 && y > 1);
+console.log(!(x === y));
 
-// // aakhir truthy value return kare ga
-// let test2 = true && "shehzad";
-// let test3 = "shehzad" && true;
-// let test4 = null && "shehzad" && 56;
-// let test = "shehzad" && null && 56;
-// let test5 = true && "shehzad" && 56;
-// let test6 = true && 0 && 56;
-// let test7 = "hh" && null;
-// let test8 = "hh" && "kk";
+// aakhir truthy value return kare ga
+console.log(true && "shehzad");
+console.log(true && "");
+console.log("shehzad" && true && "smit");
+console.log(null && "shehzad" && 56);
+console.log(null && "" && 0);
+console.log(54 && 55 && 56);
+console.log(true && "shehzad" && 56);
+console.log(true && 0 && 56);
+console.log(true && 1 && false);
+console.log("hh" && null);
+console.log("hh" && "kk");
 
-// // Phale truthy value return kare ga
-// let test9 = "shehzad" || true;
-// let test10 = "" || null;
-// let test11 = "" || true;
-// let test12 = "shehzad" || 0 || null || true;
-// let test13 = null || "shehzad" || 56;
-// let test14 = true || "shehzad" || 56;
-// let test15 = false || 0 || 56;
-// let test16 = "hh" || null;
-// let test17 = "hh" || "kk";
+// Phale truthy value return kare ga
+console.log("shehzad" || true);
+console.log("" || null);
+console.log("" || true);
+console.log("shehzad" || 0 || null || true);
+console.log(null || " " || 56);
+console.log(true || "shehzad" || 56);
+console.log(false || 0 || 56);
+console.log("hh" || null);
+console.log("hh" || "kk");
 
 // Trick
-// false || false || false;
-// true || true || true;
-// false && false && false;
-// true && true && true;
+false || false || false;
+true || true || true;
+false && false && false;
+true && true && true;
--- 03.JavaScript/08.if-else/app.js
+++ 03.JavaScript/08.if-else/app.js
@@ -1,3 +1,34 @@
+let food = true;
+let tea = true;
+let drink = true;
+
+if (food || tea || drink) {
+  console.log("le aao");
+} else {
+  console.log("ghar jao");
+}
+
+//
+
+let isEnroll = true;
+let passed = true;
+let age = 20;
+
+if (isEnroll && passed && age > 18) {
+  console.log("Pharai karo class ma");
+} else {
+  console.log("ghar jao");
+}
+// if ya else ma se koi ak chalta hy dono ak sath kabhi kabhi bhi nhi chal sagty
+
+// Nested
+
+
+
+
+// ------------------------
+
+
 // let age = 20;
 
 // let a = age - 18;
@@ -8,4 +39,3 @@
 //   console.log("sabar karo!");
 // }
 
-// if ya else ma se koi ak chalta hy dono ak sath kabhi kabhi bhi nhi chal sagty
