773b61bf9f32729736c77c642f419b1fa7c31dbd
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-07-26 10:25:49 +0500

    CLASS: Nested loop

---
+++ 03.JavaScript/12.loop/matrix.js
@@ -0,0 +1,26 @@
+const matrix1 = [
+  [8, 5],
+  [2, 3],
+];
+
+const matrix2 = [
+  [9, 5],
+  [1, 2],
+];
+
+// console.log(matrix1[0][0] + matrix2[0][0]);
+// console.log(matrix1[0][1] + matrix2[0][1]);
+
+// console.log(matrix1[1][0] + matrix2[1][0]);
+// console.log(matrix1[1][1] + matrix2[1][1]);
+
+// i = 3
+// j = 0
+
+for (let i = 0; i < 5; i++) {
+
+    for (let j = 0; j < 4; j++) {
+      console.log("Shehzad");
+    }
+
+}
