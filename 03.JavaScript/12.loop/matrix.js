68bc969ca247613019c0771ed560c31653b958f3
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2026-07-28 19:20:14 +0500

    Update matrix.js

--- 03.JavaScript/12.loop/matrix.js
+++ 03.JavaScript/12.loop/matrix.js
@@ -1,12 +1,20 @@
 const matrix1 = [
-  [8, 5],
-  [2, 3],
+  [8, 5, 4],
+  [2, 3, 0],
+  [4, 1, 9],
 ];
 
 const matrix2 = [
-  [9, 5],
-  [1, 2],
+  [11, 2, 7],
+  [9, 5, 3],
+  [5, 2, 1],
 ];
+
+// [
+//   [19, 7, 11],
+//   [11, 8, 3],
+//   [9, 3, 10]
+// ]
 
 // console.log(matrix1[0][0] + matrix2[0][0]);
 // console.log(matrix1[0][1] + matrix2[0][1]);
@@ -17,10 +25,28 @@ const matrix2 = [
 // i = 3
 // j = 0
 
-for (let i = 0; i < 5; i++) {
+let result = [];
 
-    for (let j = 0; j < 4; j++) {
-      console.log("Shehzad");
+for (let i = 0; i < matrix1.length; i++) {
+  for (let j = 0; j < matrix1[i].length; j++) {
+    let sumValue = matrix1[i][j] + matrix2[i][j];
+
+    if (result[i] === undefined) {
+      result[i] = [];
     }
-
+    result[i].push(sumValue);
+  }
 }
+console.log("Result Array: ", result);
+
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < 4; j++) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < 4; j++) {
+//     console.log(j);
+//   }
+// }
