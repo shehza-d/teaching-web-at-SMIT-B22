8398348b0b8e451623a6d90e3dea756b11ba294c
shehzaddiqbal@gmail.com <shehzaddiqbal@gmail.com>
2026-08-28 19:33:31 +0500

    CLASS: All about timezones

---
+++ 03.JavaScript/20.timestamp/app.js
@@ -0,0 +1,16 @@
+// let date = new Date(2025, 5, 23);
+let date = new Date();
+
+date = date.getUTCHours()
+
+//
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+// console.log(date.toISOString());
+
+console.log(date);
+
+let a = 1000 * 60 * 60
+
+console.log(a);
