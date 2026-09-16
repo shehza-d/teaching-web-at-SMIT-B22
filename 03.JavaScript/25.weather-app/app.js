68a71a9fce7522d6a9442f3c0834faf5db0fa692
shehzaddiqbal@gmail.com <shehzaddiqbal@gmail.com>
2026-09-16 18:46:49 +0500

    try catch, error handling

--- 03.JavaScript/25.weather-app/app.js
+++ 03.JavaScript/25.weather-app/app.js
@@ -3,17 +3,36 @@
 const para = document.querySelector("#show-weather");
 const humidity = document.querySelector("#show-humidity");
 
+// // await ko use karne ky liye functions ko async bana lazmi hy
 async function getWeather(event) {
-  event.preventDefault();
+  try {
+    event.preventDefault();
 
-  const city = document.querySelector("#city-input").value;
+    const city = document.querySelector("#city-input").value;
 
-  const response = await axios(
-    `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${city}`,
-  );
+    const response = await axios(
+      `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${city}`,
+    );
 
-  console.log(response);
+    console.log(response);
 
-  para.innerHTML = response.data.current.temp_c;
-  humidity.innerHTML = "Humidity: " +response.data.current.humidity;
+    para.innerHTML = "Temperature: " + response.data.current.temp_c;
+    humidity.innerHTML = "Humidity: " + response.data.current.humidity;
+  } catch (err) {
+    console.log(err.response.data.error.message);
+
+    para.innerHTML = err.response.data.error.message;
+  }
 }
+
+// try {
+
+//   throw new Error("Balance kam hy bhai!");
+
+// } catch (error) {
+//   console.error("Error line 26 - app.js: ", error);
+// } finally {
+//   console.log("hello");
+// }
+
+console.log("File running till the end");
